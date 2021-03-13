23a4bc1f615ad247eec9941cee97c4966788d89d
Felipe Gutierrez <felipeg@ibm.com>
2021-03-13 14:23:30 -0500

    updated

--- todo/src/main/java/com/example/todo/ToDoRepository.java
+++ todo/src/main/java/com/example/todo/ToDoRepository.java
@@ -1,6 +1,8 @@
 package com.example.todo;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ToDoRepository extends CrudRepository<ToDo,String> {
 }
